8df60bffc089ae90c1d1e1f68f9defab37a85ba7
sseusti <stepan.s7ep@gmail.com>
2025-09-16 09:07:47 +0300

    add multyply

--- calculator.py
+++ calculator.py
@@ -1,7 +1,16 @@
-def sum():
+def summary():
+    nums = get_nums()
+    print(f"total sum is = {sum(nums)}")
+    
+def multyply():
+    nums = get_nums()
+    print(f"work is = {nums[0] * nums[1]}")
+   
+   
+def get_nums():
     num1 = int(input("Enter the first number: "))
     num2 = int(input("Enter the second number: "))
-    print(f"total sum is = {num1+num2}")
-    
-sum()
+    return [num1, num2] 
+
+multyply()
     
