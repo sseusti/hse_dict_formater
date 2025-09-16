26ae92c308439e5ea087b56821963c85047192c4
Tim-M-U <timofejminaev361@gmail.com>
2025-09-16 09:57:11 +0300

    add def print_hello

---
+++ Tim.py
@@ -0,0 +1,16 @@
+def calculate_sum():
+    num1 = int(input('Enter num1: '))
+    num2 = int(input('Enter num2: '))
+    res = f'Sum of num1 and num2 is {num1 + num2}'
+    return res
+print(calculate_sum())
+
+def calculate_multiply():
+    num1 = int(input('Enter num1: '))
+    num2 = int(input('Enter num2: '))
+    res = f'Multiply num1 and num2 is: '
+    return res
+print(calculate_multiply())
+
+def print_hello():
+    print('hello')
